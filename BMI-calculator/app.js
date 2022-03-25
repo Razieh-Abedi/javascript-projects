1a4c7e3627d1014062da13ed4cc7b4811fc2cdf7
razinewworld <raziyeabedi@gmail.com>
2022-03-25 12:57:07 +0800

    modified the appreance of the form

--- BMI-calculator/app.js
+++ BMI-calculator/app.js
@@ -4,22 +4,26 @@ let female = (document.getElementById("Female").selected = true);
 let male = (document.getElementById("Male").selected = true);
 let height = document.getElementById("input-height").value;
 let weight = document.getElementById("input-weight").value;
-let bmiResult = weightValue / (heightValue * heightValue);
 
-if (age >= 18) {
-  if (bmiResult < 16) {
-    result = "Dear " + name + " , you are severely thin.";
-  } else if (16 <= bmiResult < 17)
-    result = "Dear " + name + " , you are moderately thin.";
-  else if (17 <= bmiResult < 18.5)
+function bmiCalculator(weight, height) {
+  return weight / (height * height);
+}
+
+function adultBmiResult(bmiCalculator, name, age) {
+  if ((age > 18) & (bmiCalculator > 16))
+    return "Dear " + name + " , you are severely thin.";
+  else if ((age > 18) & (16 <= bmiCalculator < 17))
+    return "Dear " + name + " , you are moderately thin.";
+  else if ((age > 18) & (17 <= bmiCalculator < 18.5))
     return "Dear " + name + " , you are mildly thin.";
-  else if (18.5 <= bmiResult < 25) return "Dear " + name + " , you are normal.";
-  else if (25 <= bmiResult < 30)
+  else if ((age > 18) & (18.5 <= bmiCalculator < 25))
+    return "Dear " + name + " , you are normal.";
+  else if ((age > 18) & (25 <= bmiCalculator < 30))
     return "Dear " + name + " , you are overweight.";
-  else if (30 <= bmiResult < 35)
+  else if ((age > 18) & (30 <= bmiCalculator < 35))
     return "Dear " + name + " , you are obese class one.";
-  else if (35 <= bmiResult < 40)
+  else if ((age > 18) & (35 <= bmiCalculator < 40))
     return "Dear " + name + " , you are obese class two.";
-  else if (40 <= bmiResult)
+  else if ((age > 18) & (40 <= bmiCalculator))
     return "Dear " + name + " , you are obese class three.";
 }
