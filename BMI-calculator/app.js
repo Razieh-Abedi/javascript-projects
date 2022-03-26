9fc51c7fc48f2e6b6fd0aebee858267583e1bf5f
razinewworld <raziyeabedi@gmail.com>
2022-03-26 16:45:40 +0800

    modified the bmi calculator

--- BMI-calculator/app.js
+++ BMI-calculator/app.js
@@ -1,36 +1,42 @@
-let name = document.getElementById("input-name").value;
-let age = document.getElementById("input-age").value;
+let name1 = document.getElementById("input-name").value;
+let age1 = document.getElementById("input-age").value;
 let female = (document.getElementById("Female").selected = true);
 let male = (document.getElementById("Male").selected = true);
 let height = document.getElementById("input-height").value;
 let weight = document.getElementById("input-weight").value;
 let result = document.querySelector("#result");
-let bmiCalculator = weight / (height * height);
+let bmiCalculator1 = weight / (height * height);
 
-// function adultBmiResult(bmiCalculator, name, age) {
-//   if ((age > 18) & (bmiCalculator > 16))
-//     return "Dear " + name + " , you are severely thin.";
-//   else if ((age > 18) & (16 <= bmiCalculator < 17))
-//     return "Dear " + name + " , you are moderately thin.";
-//   else if ((age > 18) & (17 <= bmiCalculator < 18.5))
-//     return "Dear " + name + " , you are mildly thin.";
-//   else if ((age > 18) & (18.5 <= bmiCalculator < 25))
-//     return "Dear " + name + " , you are normal.";
-//   else if ((age > 18) & (25 <= bmiCalculator < 30))
-//     return "Dear " + name + " , you are overweight.";
-//   else if ((age > 18) & (30 <= bmiCalculator < 35))
-//     return "Dear " + name + " , you are obese class one.";
-//   else if ((age > 18) & (35 <= bmiCalculator < 40))
-//     return "Dear " + name + " , you are obese class two.";
-//   else if ((age > 18) & (40 <= bmiCalculator))
-//     return "Dear " + name + " , you are obese class three.";
-// }
+console.log(adultBmiResult(bmiCalculator1,name1,age1));
 
 function adultBmiResult(bmiCalculator, name, age) {
-  if ((age > 18) {
-      if (bmiCalculator > 16) result.innerHTML = "Dear " + name + " , you are severely thin.";
-      else if (16 <= bmiCalculator < 17) result.innerHTML = "Dear " + name + " , you are moderately thin.";
-  }
-  else if result.innerHTML = "Dear " + name + " , you are normal.";
+  if ((age > 18) & (bmiCalculator > 16))
+    return "Dear " + name + " , you are severely thin.";
+  else if ((age > 18) & (16 <= bmiCalculator < 17))
+    return "Dear " + name + " , you are moderately thin.";
+  else if ((age > 18) & (17 <= bmiCalculator < 18.5))
+    return "Dear " + name + " , you are mildly thin.";
+  else if ((age > 18) & (18.5 <= bmiCalculator < 25))
+    return "Dear " + name + " , you are normal.";
+  else if ((age > 18) & (25 <= bmiCalculator < 30))
+    return "Dear " + name + " , you are overweight.";
+  else if ((age > 18) & (30 <= bmiCalculator < 35))
+    return "Dear " + name + " , you are obese class one.";
+  else if ((age > 18) & (35 <= bmiCalculator < 40))
+    return "Dear " + name + " , you are obese class two.";
+  else if ((age > 18) & (40 <= bmiCalculator))
+    return "Dear " + name + " , you are obese class three.";
 }
-  
+
+function adultBmiResult2(bmiCalculator, name, age) {
+  if (age > 18) {
+    if (bmiCalculator > 16) {
+      result.innerText = "Dear " + name + " , you are severely thin.";
+    } else if (16 <= bmiCalculator < 17) {
+      result.innerText = "Dear " + name + " , you are moderately thin.";
+    }
+
+  } else {
+    result.innerText = "Dear " + name + " , you are normal.";
+  }
+}
