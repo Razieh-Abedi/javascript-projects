a090ae72501221754346bf4a5f606247b86fe673
razinewworld <raziyeabedi@gmail.com>
2022-04-05 17:49:35 +0800

    finished the meter/sectimeter unit convertor and created the multi unit convetor

--- unit-convertor/app.js
+++ unit-convertor/app.js
@@ -1,10 +1,27 @@
-document.querySelector(".arrow-icon").addEventListener("click", () => {
+// document.querySelector(".result-arrow").addEventListener("click", () => {
+//   let centimeter = document.getElementById("centimeter");
+//   let meter = document.getElementById("meter");
+//   let metreToCentimeter = meter.value * 100;
+//   let centimeterToMeter = centimeter.value / 100;
+//   centimeter.value = metreToCentimeter;
+//   meter.value = centimeterToMeter;
+// });
+function inchCentimeter(inch, centimeter) {
+  if (inch.selected == true && centimeter.selected == true) {
+    resultLeft.value = centimeterToInch;
+    resultRight.value = inchToCentimeter;
+  }
+}
+document.querySelector(".result-arrow").addEventListener("click", () => {
+  let inch = document.getElementById("inch");
   let centimeter = document.getElementById("centimeter");
-  let centimeterValue = centimeter.value;
+  let foot = document.getElementById("foot");
   let meter = document.getElementById("meter");
-  let meterValue = meter.value;
-  let metreToCentimeter = meterValue * 100;
-  let centimeterToMeter = centimeterValue / 100;
-  centimeterValue = metreToCentimeter;
-  meterValue = centimeterToMeter;
+  let mile = document.getElementById("mile");
+  let kilometer = document.getElementById("kilometer");
+  let resultLeft = document.getElementById("result-text-left");
+  let resultRight = document.getElementById("result-text-right");
+  let inchToCentimeter = inch.value * 2.54;
+  let centimeterToInch = centimeter.value / 2.54;
+  inchCentimeter(inch, centimeter);
 });
